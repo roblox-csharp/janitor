22f6d5dc90b332927dec11b74209bf59368acd65
Riley Peel <49625808+R-unic@users.noreply.github.com>
2025-01-02 01:13:47 -0500

    chore: extern everything

--- Janitor.cs
+++ Janitor.cs
@@ -19,92 +19,24 @@ namespace JanitorModule
         public bool CurrentlyCleaning { get; }
         public bool SuppressInstanceReDestroy { get; set; }
 
-        public static bool Is(object obj)
-        {
-            return default;
-        }
+        public static extern bool Is(object obj);
 
-        public IDisconnectable Add(IDisconnectable obj, string? methodName = null, I? index = default)
-        {
-            return default!;
-        }
-
-        public IDestroyable Add(IDestroyable obj, string? methodName = null, I? index = default)
-        {
-            return default!;
-        }
-
-        public Action Add(Action obj, string? methodName = null, I? index = default)
-        {
-            return default!;
-        }
-
-        public object Add(object obj, string? methodName = null, I? index = default)
-        {
-            return default!;
-        }
-
-        public object AddObject(Type obj, string? methodName = null, I? index = default)
-        {
-            return default;
-        }
-
-        public T AddObject<T>(Type obj, string? methodName = null, I? index = default) where T : notnull
-        {
-            return default!;
-        }
-
-        public Janitor Remove(I index)
-        {
-            return null!;
-        }
-
-        public Janitor RemoveNoClean(I index)
-        {
-            return null!;
-        }
-
-        public Janitor RemoveList(params I[] indices)
-        {
-            return null!;
-        }
-
-        public Janitor RemoveListNoClean(params I[] indices)
-        {
-            return null!;
-        }
-
-        public object? Get(I index)
-        {
-            return default;
-        }
-
-        public T? Get<T>(I index) where T : notnull
-        {
-            return default;
-        }
-
-        public IDictionary<I, T> GetAll<T>() where T : notnull
-        {
-            return null!;
-        }
-
-        public void Cleanup()
-        { 
-        }
-
-        public void Destroy()
-        {
-        }
-
-        public ScriptConnection LinkToInstance(Instance instance, bool allowMultiple)
-        {
-            return null!;
-        }
-
-        public Janitor LinkToInstances(params Instance[] instances)
-        {
-            return null!;
-        }
+        public extern IDisconnectable Add(IDisconnectable obj, string? methodName = null, I? index = default);
+        public extern IDestroyable Add(IDestroyable obj, string? methodName = null, I? index = default);
+        public extern Action Add(Action obj, string? methodName = null, I? index = default);
+        public extern object Add(object obj, string? methodName = null, I? index = default);
+        public extern object AddObject(Type obj, string? methodName = null, I? index = default);
+        public extern T AddObject<T>(Type obj, string? methodName = null, I? index = default) where T : notnull;
+        public extern Janitor Remove(I index);
+        public extern Janitor RemoveNoClean(I index);
+        public extern Janitor RemoveList(params I[] indices);
+        public extern Janitor RemoveListNoClean(params I[] indices);
+        public extern object? Get(I index);
+        public extern T? Get<T>(I index) where T : notnull;
+        public extern IDictionary<I, T> GetAll<T>() where T : notnull;
+        public extern void Cleanup();
+        public extern void Destroy();
+        public extern ScriptConnection LinkToInstance(Instance instance, bool allowMultiple);
+        public extern Janitor LinkToInstances(params Instance[] instances);
     }
 }
